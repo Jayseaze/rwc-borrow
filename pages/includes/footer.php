c777eb96e4b4265910159b0b2aa0cac759629b84
Jacoppz <123076794+jacoppz@users.noreply.github.com>
2023-02-11 14:58:34 +0700

    update last

--- pages/includes/footer.php
+++
@@ -1,9 +0,0 @@
-<?php 
-    
-    //require_once('../authen.php'); 
-?>
-<footer class="main-footer">
-    <strong>Copyright &copy; 2021 - 2023
-        <a > RWS-BORROW PROJECT </a>.
-    </strong> All rights reserved.
-</footer>
